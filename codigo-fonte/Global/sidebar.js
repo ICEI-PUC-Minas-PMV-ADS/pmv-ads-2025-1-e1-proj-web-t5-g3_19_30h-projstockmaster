f04f7aeb093447acfbef6a657d45e3d8b8c42ca2
jjpxt <jotajunior.peixoto@gmail.com>
2025-05-03 17:55:15 -0300

    att

--- codigo-fonte/Global/sidebar.js
+++ codigo-fonte/Global/sidebar.js
@@ -29,7 +29,7 @@ class SidebarManager {
         </div>
         <ul class="nav-menu">
           <li class="nav-item">
-            <a href="dashboard.html" class="nav-link" data-target="dashboard">Dashboard</a>
+            <a href="../Dashboard/dashboard.html" class="nav-link" data-target="dashboard">Dashboard</a>
           </li>
           <li class="nav-item">
             <a href="../paginaGestaoDeProdutos/gestaoProd.html" class="nav-link" data-target="gestaoProd">Gestão de produtos</a>
